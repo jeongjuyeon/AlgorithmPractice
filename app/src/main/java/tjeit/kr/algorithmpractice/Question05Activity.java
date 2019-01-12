902b96fc93bf1c30090df6c0fb77a6683a0ef886
dajeongpark <ekwjd37891@naver.com>
2019-01-12 14:41:08 +0900

    야구게임 문제 숨김

--- app/src/main/java/tjeit/kr/algorithmpractice/Question05Activity.java
+++ app/src/main/java/tjeit/kr/algorithmpractice/Question05Activity.java
@@ -1,16 +1,33 @@
 package tjeit.kr.algorithmpractice;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tjeit.kr.algorithmpractice.adapters.ChatAdapter;
+import tjeit.kr.algorithmpractice.datas.Chat;
+
 public class Question05Activity extends BaseActivity {
+
+    //    굳이 <Chat> 을 명시하는 이유?
+//    자바 1.8로 오면서 안써도 되게 업데이트.
+//    올드버젼을 쓰는 경우 대비.
+    List<Chat> chatList = new ArrayList<Chat>();
+
+    ChatAdapter mAdapter;
 
     //    컴퓨터가 출제한 문제 (3개의 숫자가 저장)
     int[] questionIntArray = new int[3];
+
+    //    사용자가 몇번이나 정답을 시도했는지
+    int userTryCount = 0;
 
 
     private android.widget.ListView chatListView;
@@ -32,78 +49,141 @@ public class Question05Activity extends BaseActivity {
 
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 checkUserNumber();
             }
         });
 
-
-
     }
 
-    void checkUserNumber (){
-        if (numInputEdt.length() != 3){
-            Toast.makeText(mContext, " 3자리숫자를 입력 하세요", Toast.LENGTH_SHORT).show();
+    //    ?S ?B인지 체크하는 메쏘드
+    void checkUserNumber() {
+
+//        시도 횟수를 1 증가.
+        userTryCount++;
+
+//        세자리가 아닐경우 다시 입력하게 Toast
+
+        if (numInputEdt.length() != 3) {
+            Toast.makeText(mContext, "3자리 숫자를 입력하세요.", Toast.LENGTH_SHORT).show();
             return;
         }
 
 //        사용자가 입력한 숫자를 저장할 세칸짜리 배열.
-//         152 => [1], [5], [2]
-        int[] userInputArray = new int[3];
+//        152 => [1],[5],[2]
+        int[] userInputIntArray = new int[3];
 
 //        사용자가 입력한 값을 String으로 따내자.
         String inputStr = numInputEdt.getText().toString();
 
+//        입력한 값을 채팅처럼 메세지로 출력. => chatList에 추가.
+        Chat inputNumChat = new Chat("user", inputStr);
+        chatList.add(inputNumChat);
+        mAdapter.notifyDataSetChanged();
+//        notifyDatasetChanged 필요함!
+
 //        배열에 각 자리의 숫자를 집어넣기.
-//        먼저, String -> 숫자로 변경. "512" => 512
+
+//        먼저, String -> 숫자로 변경. "512" => 512 % 100 / 10 = 12 /10 = 1
         int inputNumber = Integer.parseInt(inputStr);
 
 //        0번칸 : 맨 앞자리를 대입하는 방법?
-         userInputArray[0] = inputNumber / 100;
-
+        userInputIntArray[0] = inputNumber / 100;
 
 //        1번칸 : 가운데 자리를 따내는 방법?
-         userInputArray[1] = inputNumber % 100 / 10;
+        userInputIntArray[1] = inputNumber % 100 / 10;
 
 //        2번칸 : 맨 뒷자리를 따내는 방법?
-        userInputArray[2] = inputNumber % 10;
+        userInputIntArray[2] = inputNumber % 10;
 
-//        숫자를 다 집어넣었으면, 실제로 정답과 비교해서 ?S, ?B인지 계산.
 
-//        ?S인지 저장될 변수
+//        숫자를 다 집어넣었으면, 실제로 정답과 비교해서 ?S ?B인지 계산.
+
+        // ?S인지 저장될 변수
         int strikeCount = 0;
 //        ?B인지 저장될 변수
-        int ballcount = 0;
-
+        int ballCount = 0;
 
 //        i : 사용자가 입력한 값을 담당하는 index
-        for (int i=0; i<3; i++){
+        for (int i=0 ; i < 3; i++) {
 
 //            j : 정답지를 돌아보는 index
-            for (int j=0; j<3; i++) {
-                if (userInputArray[i] == questionIntArray[j]) {
-                    if (i == j){
+            for (int j=0 ; j<3 ; j++) {
+
+//                숫자가 같은걸 발견!
+                if (userInputIntArray[i] == questionIntArray[j]) {
+                    if (i == j) {
 //                        위치도 같다! strike 갯수 증가
                         strikeCount++;
                     }
-                    else{
+                    else {
 //                        숫자는 같지만, 위치는 다르다!
 //                        ball 카운트를 증가.
-                        ballcount++;
+
+                        ballCount++;
                     }
+
                 }
+
             }
 
         }
 
 //        임시로 토스트에 S/B 출력
-        String temp = String.format("%d S %d B 입니다.", strikeCount, ballcount);
-        Toast.makeText(mContext, temp, Toast.LENGTH_SHORT).show();
+        final String replyMessage = String.format("%d S %d B 입니다.", strikeCount, ballCount);
+//        Toast.makeText(mContext, temp, Toast.LENGTH_SHORT).show();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Chat reply = new Chat("computer", replyMessage);
+                chatList.add(reply);
+                mAdapter.notifyDataSetChanged();
+                chatListView.smoothScrollToPosition(chatList.size()-1);
+            }
+        }, 400);
+
+
+
+
+        if (strikeCount == 3) {
+//            Toast.makeText(mContext, "정답입니다.!", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Chat correct = new Chat("computer", "정답입니다!");
+                    chatList.add(correct);
+                    mAdapter.notifyDataSetChanged();
+                    chatListView.smoothScrollToPosition(chatList.size()-1);
+                }
+            }, 700);
+
+
+//            Toast.makeText(mContext, userTryCount+"번 만에 맞췄습니다.", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Chat countMessage = new Chat("computer", userTryCount+"번 만에 맞췄습니다.");
+                    chatList.add(countMessage);
+                    mAdapter.notifyDataSetChanged();
+                    chatListView.smoothScrollToPosition(chatList.size()-1);
+                }
+            }, 1000);
+
+
+        }
 
     }
 
     @Override
     public void setValues() {
+
+        mAdapter = new ChatAdapter(mContext, chatList);
+        chatListView.setAdapter(mAdapter);
 
 //        화면을 키면 컴퓨터가 바로 문제를 출제.
         makeQuestionNumbers();
@@ -146,9 +226,9 @@ public class Question05Activity extends BaseActivity {
         }
 
 //        임시로 값을 확인
-        String temp = String.format("%d%d%d", questionIntArray[0], questionIntArray[1], questionIntArray[2]);
-
-        numInputEdt.setText(temp);
+//        String temp = String.format("%d%d%d", questionIntArray[0], questionIntArray[1], questionIntArray[2]);
+//
+//        numInputEdt.setText(temp);
 
     }
 
---
+++ app/src/main/java/tjeit/kr/algorithmpractice/adapters/ChatAdapter.java
@@ -0,0 +1,69 @@
+package tjeit.kr.algorithmpractice.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import tjeit.kr.algorithmpractice.R;
+import tjeit.kr.algorithmpractice.datas.Chat;
+
+public class ChatAdapter extends ArrayAdapter<Chat> {
+
+    Context mContext;
+    List<Chat> mList;
+    LayoutInflater inf;
+
+    public ChatAdapter(Context context, List<Chat> list) {
+        super(context, R.layout.chat_list_item, list);
+
+        mContext = context;
+        mList = list;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.chat_list_item, null);
+        }
+
+        LinearLayout computerMsgLayout = row.findViewById(R.id.computerMsgLayout);
+        TextView computerMsgTxt = row.findViewById(R.id.computerMsgTxt);
+        LinearLayout userMsgLayout = row.findViewById(R.id.userMsgLayout);
+        TextView userMsgTxt = row.findViewById(R.id.userMsgTxt);
+
+        Chat data = mList.get(position);
+
+//        data가 가진 sender를 봤을때 computer / user 두가지 경우가 존재.
+
+        if (data.getSender().equals("user")) {
+//            사용자가 입력한 메세지
+
+            computerMsgLayout.setVisibility(View.GONE);
+            userMsgLayout.setVisibility(View.VISIBLE);
+
+            userMsgTxt.setText(data.getMessage());
+
+        }
+        else {
+//            컴퓨터가 입력한 메세지
+
+            userMsgLayout.setVisibility(View.GONE);
+            computerMsgLayout.setVisibility(View.VISIBLE);
+
+            computerMsgTxt.setText(data.getMessage());
+
+        }
+
+        return row;
+    }
+}
